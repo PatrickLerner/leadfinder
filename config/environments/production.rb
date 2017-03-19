686cda688acc7294da124bc0b9811db28f4b77e3
Patrick Lerner <patricklerner@posteo.de>
2017-03-19 12:45:39 +0100

    allow web sockets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [
+    %r{https?://138.68.89.100},
+    %r{https?://leadfinder.patricklerner.com}
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
