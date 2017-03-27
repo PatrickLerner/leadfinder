3b02e820bad4bc06d7bc16d083c0d329426f4e4b
Patrick Lerner <patricklerner@posteo.de>
2017-03-27 23:22:35 +0200

    Update extension version

--- app/javascript/packs/pages/dashboard.jsx
+++ app/javascript/packs/pages/dashboard.jsx
@@ -17,7 +17,7 @@ export default class Dashboard extends Component {
               </p>
             </div>
             <div className='col-4'>
-              <a className='button' href='https://leadfinder.patricklerner.com/extension/leadfinder_chrome_2.crx'>
+              <a className='button' href='https://leadfinder.patricklerner.com/extension/leadfinder_chrome_3.crx'>
                 <i className='fa fa-fw fa-chrome'></i> Download Chrome Extension
               </a>
             </div>
