005f35065bd9ff80898aec3451adf1c2b594f8a4
Patrick Lerner <patricklerner@posteo.de>
2017-04-18 02:19:44 +0200

    Ignore development errors

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,6 @@
 Airbrake.configure do |config|
+  c.environment = Rails.env
+  c.ignore_environments = %w(development test cucumber)
   config.host = 'https://exceptions.codebasehq.com'
   config.project_key = 'cb832a9a-720e-2b40-daec-c494f947f8ea'
   config.project_id = 176_161
