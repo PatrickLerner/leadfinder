71d4fe49ea089e8056a11b96c2cd9a1d33ad0ad1
Patrick Lerner <patricklerner@posteo.de>
2017-04-24 19:39:50 +0200

    Refactor CVR a bit

--- spec/requests/entry_retrieve_spec.rb
+++ spec/requests/entry_retrieve_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
-  config.ignore_localhost = true
-end
 
 describe 'Access via API to add a new entry', type: :request do
   let(:entry_attributes) do
@@ -17,12 +10,21 @@ describe 'Access via API to add a new entry', type: :request do
 
   let(:body) { JSON.parse(response.body).with_indifferent_access }
 
-  it 'allow to add an entry and it resolves it inline' do
+  before(:each) do
     VCR.use_cassette('peter_launchwerk') do
       post '/api/v1/entries/retrieve', params: { entry: entry_attributes }, headers: { 'X-API-KEY': api_key }
-      expect(response).to be_successful
-      expect(body[:entry][:first_name]).to eq('Peter')
-      expect(body[:entry][:email]).to eq('peter@launchwerk.de')
     end
+  end
+
+  it 'allow to add an entry and it resolves it inline' do
+    expect(response).to be_successful
+    expect(body[:entry][:first_name]).to eq('Peter')
+    expect(body[:entry][:email]).to eq('peter@launchwerk.de')
+  end
+
+  it 'actually persists the entry' do
+    found_entry = Entry.find_by(id: body[:entry][:id])
+    expect(found_entry).to be_present
+    expect(found_entry.user).to eq(user)
   end
 end
