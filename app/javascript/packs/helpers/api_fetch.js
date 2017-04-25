cd337ba4b4e461c43ad3a8b0bec43308b44413a5
Patrick Lerner <patricklerner@posteo.de>
2017-04-25 07:58:24 +0200

    Redirect web interface to sign in view if user becomes logged out or unauthorized (not through the interface)
    [touch: 31]

--- app/javascript/packs/helpers/api_fetch.js
+++ app/javascript/packs/helpers/api_fetch.js
@@ -1,12 +1,13 @@
 import fetchDefaults from 'fetch-defaults'
 import 'whatwg-fetch'
+import { browserHistory } from 'react-router';
 
 function getCookieValue(a) {
   const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
   return b ? b.pop() : '';
 }
 
-export const apiFetch = fetchDefaults(fetch, {
+const leadApiFetch = fetchDefaults(fetch, {
   headers: {
     'Accept': 'application/json, text/plain, */*',
     'Content-Type': 'application/json',
@@ -14,3 +15,16 @@ export const apiFetch = fetchDefaults(fetch, {
   },
   credentials: 'same-origin'
 });
+
+export const apiFetch = (url, options) => {
+  return new Promise((resolve, error) => {
+    return leadApiFetch(url, options).then(result => {
+      if (result.status === 401) {
+        error(result);
+        browserHistory.replace('/');
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
